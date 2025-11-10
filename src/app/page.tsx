7351575df811145201a13b052ed9352930c2c77b
VSFloriano <noit3xescur4@gmail.com>
2025-11-10 09:43:46 -0300

    Feat: completed static ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,132 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import {
+  Plus,
+  List,
+  Check,
+  CircleAlert,
+  SquarePen,
+  Trash,
+  ListCheck,
+  Sigma,
+} from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 const home = () => {
-  return(
-        <div>Vini</div>
-  )
-}
+  return (
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg">
+        <CardContent className="flex gap-2">
+          <Input placeholder="adicionar tarefa" />
+          <Button variant="default" className="cursor-pointer">
+            {" "}
+            <Plus />
+            Cadastrar
+          </Button>
+        </CardContent>
+        <CardContent>
+          <Separator className="mb-2" />
+          <div className="m-2 flex gap-2">
+            <Badge className="cursor-pointer" variant="default">
+              <List /> Todas
+            </Badge>
+            <Badge className="cursor-pointer" variant="outline">
+              <CircleAlert /> Não Finalizadas
+            </Badge>
+            <Badge className="cursor-pointer" variant="outline">
+              <Check /> Concluidas
+            </Badge>
+          </div>
 
-export default home
+          <div className="mt-4 border-b-1">
+            <div className="h-14 flex justify-between items-center border-b-1 border-t-1">
+              <div className="bg-green-300 w-1 h-full"></div>
+              <p className="flex-1 px-2 text-sm">Estudar</p>
+              <div className="flex items-center gap-2">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <SquarePen size={16} className="cursor-pointer" />
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar Tarefa</DialogTitle>
+                    </DialogHeader>
+
+                    <div className="flex gap-2">
+                      <Input placeholder="Editar Tarefa" />
+                      <Button className="cursor-pointer">Editar</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2 items-center">
+              <ListCheck size={18} />
+              <p className="text-xs">Tarefas concluidas (3/3)</p>
+            </div>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  className="text-xs h-7 cursor-pointer"
+                  variant={"outline"}
+                >
+                  <Trash /> Limpar tarefas concluidas
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja excluir X itens?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div
+              className="h-full bg-blue-500 rounded-md"
+              style={{ width: "50%" }}
+            ></div>
+          </div>
+
+          <div className="flex justify-end items-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 Tarefas no total</p>
+          </div>
+        </CardContent>
+
+        <div></div>
+      </Card>
+    </main>
+  );
+};
+
+export default home;
